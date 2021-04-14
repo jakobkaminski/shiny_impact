98fa55d56deb2d39208231258e99a7a48c0e7e4f
jakobkaminski <jakobkaminski@gmx.de>
2021-04-14 15:09:26 +0200

    csv upload working

---
+++ shiny_impact/ui.R
@@ -0,0 +1,30 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+    sidebarLayout(
+        sidebarPanel(
+            fileInput("file1", "Choose CSV File",
+                      accept = c(
+                          "text/csv",
+                          "text/comma-separated-values,text/plain",
+                          ".csv")
+            ),
+            tags$hr(),
+            checkboxInput("header", "Header", TRUE)
+        ),
+        mainPanel(
+            tableOutput("contents")
+                )
+                )
+                )
+    )
